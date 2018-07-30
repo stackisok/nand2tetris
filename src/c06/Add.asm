c905d51b14ea3ae7f2c7f6366972425bd07848d4
wcj <wcj@ifourthwall.com>
2018-07-30 17:48:00 +0800

    new Assembler

---
+++ src/c06/Add.asm
@@ -0,0 +1,16 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/c06/add/Add.asm
+
+// Computes R0 = 2 + 3
+
+@2
+D=A
+@3
+D=D+A
+
+
+
+@0
+M=D

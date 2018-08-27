5b4cf4ada21cde4419091bebf697945988dee7ec
wangchenjun <dabai4@163.com>
2018-08-27 14:46:48 +0800

    some keywords(like while,if,do.. ) parse is not accomplished

--- src/c10/CompilationEngine.java
+++ src/c10/CompilationEngine.java
@@ -1,7 +1,6 @@
 package c10;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.regex.Pattern;
@@ -17,15 +16,17 @@ public class CompilationEngine {
     int position;
     String token;
     TokenType token_type;
+    TokenType next_token_type;
 
     boolean isComment;
     public static final Pattern digital = Pattern.compile("\\d+");
     private static final Pattern LETTER_PATTERN = Pattern.compile("[a-zA-Z]+");
     private static final Pattern IDENTIFIER_PATTERN = Pattern.compile("\\w+");
 
-    CompilationEngine(String fileName) throws FileNotFoundException {
+    CompilationEngine(String fileName) throws IOException {
 
         fileReader = new BufferedReader(new FileReader(fileName));
+        readNextLine();
 
     }
 
@@ -41,7 +42,7 @@ public class CompilationEngine {
         consumeSymbol(RIGHT_CURLY_BRACKET);
 
 
-        while (hasMoreTokens()) {}
+//        while (hasMoreTokens()) {}
     }
 
     private boolean isNextTokenTheSymbol(Symbol symbol) {
@@ -54,8 +55,19 @@ public class CompilationEngine {
     //获取下一个token
     private void moveNextToken() {
 
-        for (int end = position + 1; end < currentLine.length(); end++) {
+        while (position == currentLine.length()) {
+            try {
+                readNextLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        for (int end = position + 1; end < currentLine.length() + 1; end++) {
             String token_sub = currentLine.substring(position, end);
+            if (token_sub.equals(" ")) {
+                position = position + 1;
+                continue;
+            }
             if (isSymbol(token_sub)) {
                 token = token_sub;
                 token_type = SYMBOL;
@@ -64,7 +76,7 @@ public class CompilationEngine {
             }
 
             String firstTokenChar = String.valueOf(token_sub.charAt(0));
-            String nextTokenChar = String.valueOf(currentLine.charAt(end + 1));
+            String nextTokenChar = String.valueOf(currentLine.charAt(end));
 
             //token_sub is identifier
             if (token_sub.matches(LETTER_PATTERN.pattern()) && !firstTokenChar.matches(digital.pattern())) {
@@ -130,7 +142,7 @@ public class CompilationEngine {
                 while (!line.contains("*/")) {
                     line = fileReader.readLine();
                 }
-                String s = line.replaceAll("\\w*\\*/", "").trim();
+                String s = line.replaceAll(".*\\*/", "").trim();
                 isComment = false;
                 if (s.equals("")) {
                     readNextLine();
@@ -147,33 +159,51 @@ public class CompilationEngine {
             }
             if (line.contains("/*")) {
                 if (line.contains("*/")) {
-                    currentLine = line.replaceAll("/\\*\\w*\\*/","").trim();
+                    currentLine = line.replaceAll("/\\*.*\\*/","").trim();
                     position = 0;
 
                     return;
                 }
                 isComment = true;
-                currentLine = line.replaceAll("/\\*\\w*", "").trim();
+                currentLine = line.replaceAll("/\\*.*", "").trim();
                 position = 0;
+                return;
             }
+            currentLine = line.trim();
+            position = 0;
         }
 
     }
     private String getNextToken() {
 
-        for (int end = position + 1; end < currentLine.length(); end++) {
+        while (position == currentLine.length()) {
+            try {
+                readNextLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        for (int end = position + 1; end < currentLine.length() + 1; end++) {
+
             String token_sub = currentLine.substring(position, end);
+            if (token_sub.equals(" ")) {
+                position = position + 1;
+                continue;
+            }
             if (isSymbol(token_sub)) {
 
+                next_token_type = SYMBOL;
                 return token_sub;
             }
 
             String firstTokenChar = String.valueOf(token_sub.charAt(0));
-            String nextTokenChar = String.valueOf(currentLine.charAt(end + 1));
+            String nextTokenChar = String.valueOf(currentLine.charAt(end));
 
             //token_sub is identifier
             if (token_sub.matches(LETTER_PATTERN.pattern()) && !firstTokenChar.matches(digital.pattern())) {
                 if (isSymbolOrSpace(nextTokenChar)) {
+                    next_token_type = IDENTIFIER;;
+
                     return token_sub;
                 }
                 continue;
@@ -182,6 +212,7 @@ public class CompilationEngine {
             //token_sub is digital
             if (token_sub.matches(digital.pattern())) {
                 if (isSymbolOrSpace(nextTokenChar)) {
+                    next_token_type = INT_CONST;
                     return token_sub;
                 }
                 continue;
@@ -190,6 +221,7 @@ public class CompilationEngine {
             //token_sub is digital
             if (token_sub.matches(digital.pattern())) {
                 if (isSymbolOrSpace(nextTokenChar)) {
+                    next_token_type = INT_CONST;
                     return token_sub;
                 }
                 continue;
@@ -197,6 +229,7 @@ public class CompilationEngine {
             //token_sub is string
             if (firstTokenChar.equals("\"")) {
                 if (isSymbolOrSpace(nextTokenChar)) {
+                    next_token_type = STRING_CONST;
                     return token_sub;
                 }
                 continue;
@@ -205,12 +238,13 @@ public class CompilationEngine {
         return "";
     }
 
-    boolean isTokenOfType(TokenType type) {
-        if (token_type == type) {
+    boolean isNextTokenOfType(TokenType type) {
+        if (next_token_type == type) {
                 return true;
         }
         return false;
     }
+
     boolean isSymbol(String token) {
         for (Symbol e : Symbol.values()) {
             if (e.character.equals(token))
@@ -224,16 +258,16 @@ public class CompilationEngine {
 
         return 1;
     }
-    private boolean hasMoreTokens() {
-
-        return true;
-    }
+//    private boolean hasMoreTokens() {
+//
+//        return true;
+//    }
     void consumeSymbol(Symbol symbol) {
         moveNextToken();
-        if (token.equals(symbol.toString())) {
+        if (token.equals(symbol.character)) {
             writeXmlElement("symbol", token);
         } else {
-            throw new RuntimeException("the token is not expected");
+            throw new RuntimeException(symbol + " the token is not expected" + token);
         }
     }
 
@@ -250,27 +284,30 @@ public class CompilationEngine {
         //todo classVarDec{
 
         String nextToken = getNextToken();
-        if (isTokenInWords(nextToken, STATIC.toString(), FIELD.toString()))
-        consumeKeyword(STATIC, FIELD);
-        consumeType();
-        consumeIdentifier();
-        nextToken = getNextToken();
-        while (!nextToken.equals(SEMICOLON.character)) {
-            consumeSymbol(COMMA);
+        if (isNextTokenInWords(nextToken, STATIC.toString(), FIELD.toString())) {
+
+
+            consumeKeyword(STATIC, FIELD);
+            consumeType();
             consumeIdentifier();
+            nextToken = getNextToken();
+            while (!nextToken.equals(SEMICOLON.character)) {
+                consumeSymbol(COMMA);
+                consumeIdentifier();
+            }
+            consumeSymbol(SEMICOLON);
         }
-        consumeSymbol(SEMICOLON);
 
         //todo }
 
     }
 
     private void consumeType() {
-        moveNextToken();
-        if (isTokenOfType(KEYWORD)) {
+        String nextToken = getNextToken();
+        if (isNextTokenOfType(KEYWORD)) {
             consumeKeyword(INT, CHAR, BOOLEAN);
         }
-        else if (isTokenOfType(IDENTIFIER)) {
+        else if (isNextTokenOfType(IDENTIFIER)) {
             consumeIdentifier();
         }
         else {
@@ -278,29 +315,146 @@ public class CompilationEngine {
         }
     }
 
-    boolean isTokenInWords (String next_token,String... words) {
+    boolean isNextTokenInWords(String next_token, String... words) {
 
         for (String word : words) {
             if (word.equals(next_token))
                 return true;
         }
 
-        throw new RuntimeException("the token not in such words");
+        return false;
+//        throw new RuntimeException("the token not in such words");
     }
     private void parseSubroutine() {
+        //todo <>
+        String nextToken = getNextToken();
+
+        if (isNextTokenInWords(nextToken, CONSTRUCTOR.toString(), METHOD.toString(), FUNCTION.toString())) {
+            consumeKeyword(CONSTRUCTOR, FUNCTION, METHOD);
+
+        }
+        if (!nextToken.equals(CONSTRUCTOR.toString()))
+            consumeType();
+        consumeIdentifier();
+        consumeSymbol(LEFT_PARENTHESIS);
+        parseParams();
+        consumeSymbol(RIGHT_PARENTHESIS);
+        parseSubroutineBody();
+        //todo </>
 
     }
+
+    private void parseSubroutineBody() {
+        consumeSymbol(LEFT_CURLY_BRACKET);
+        String nextToken = getNextToken();
+
+        while (isNextTokenInWords(nextToken, VAR.toString())) {
+            parseVar();
+            nextToken = getNextToken();
+
+        }
+        parseStatement();
+        consumeSymbol(RIGHT_CURLY_BRACKET);
+
+    }
+
+    private void parseVar() {
+        consumeKeyword(VAR);
+        consumeType();
+        consumeIdentifier();
+        while (isNextTokenTheSymbol(COMMA)) {
+            consumeSymbol(COMMA);
+            consumeIdentifier(); // varName
+        }
+        consumeSymbol(SEMICOLON);
+
+    }
+
+    private void parseParams() {
+
+    }
+
     //such like
     public void parseStatement() {
+        while (true) {
+            String nextToken = getNextToken();
+            if (isNextTokenInWords(nextToken, LET.toString())) {
+                parseLet();
+            }
+            else if (isNextTokenInWords(nextToken, IF.toString())) {
+                parseIf();
+            }
+            else if (isNextTokenInWords(nextToken, WHILE.toString())) {
+                parseWhile();
+            }
+            else if (isNextTokenInWords(nextToken, DO.toString())) {
+                parseDo();
+            }
+            else if (isNextTokenInWords(nextToken, RETURN.toString())) {
+                parseReturn();
+            }
+            else {
+                break;
+            }
+            nextToken = getNextToken();
+        }
 
     }
-    public void parseWhileStatement() {
+
+    private void parseLet() {
+        consumeKeyword(LET);
+        consumeIdentifier();
+        if (isNextTokenTheSymbol(LEFT_SQUARE_BRACKET)) {
+            consumeSymbol(LEFT_SQUARE_BRACKET);
+            parseExpression();
+            consumeSymbol(RIGHT_SQUARE_BRACKET);
+        }
+        consumeSymbol(EQUAL);
+        parseExpression();
+        consumeSymbol(SEMICOLON);
 
     }
-    public void parseIfStatement() {
+
+    private void parseReturn() {
 
     }
-    public void parseStatementSequence() {
+
+    private void parseDo() {
+        consumeKeyword(DO);
+        consumeIdentifier();
+        consumeSymbol(DOT);
+        consumeIdentifier();
+        consumeSymbol(SEMICOLON);
+    }
+
+    public void parseWhile() {
+        consumeKeyword(WHILE);
+        consumeSymbol(LEFT_PARENTHESIS);
+        parseExpression();
+        consumeSymbol(RIGHT_PARENTHESIS);
+        consumeSymbol(LEFT_CURLY_BRACKET);
+
+        consumeSymbol(RIGHT_CURLY_BRACKET);
+
+
+
+
+    }
+    public void parseIf() {
+        consumeKeyword(IF);
+        consumeSymbol(LEFT_PARENTHESIS);
+        parseExpression();
+        consumeSymbol(RIGHT_PARENTHESIS);
+
+//        consumeStatementBlock();
+        String nextToken = getNextToken();
+        if (isNextTokenInWords(nextToken,ELSE.toString())) {
+            consumeKeyword(ELSE);
+//            consumeStatementBlock();
+        }
+
+    }
+        public void parseStatementSequence() {
 
     }
     public void parseExpression() {
@@ -314,13 +468,16 @@ public class CompilationEngine {
             writeXmlElement("keyword", token);
             return;
         }
+        String expectWords = "";
         for (Keyword key : otherwords) {
-            if (token.equals(keyword.toString())) {
+            expectWords += "," + key;
+            if (token.equals(key.toString())) {
                 writeXmlElement("keyword", token);
                 return;
             }
         }
-        throw new RuntimeException("the token is not expected");
+
+        throw new RuntimeException(keyword + " the token is not expected" + "(" + expectWords +")");
 
 
     }
@@ -328,15 +485,17 @@ public class CompilationEngine {
 
 
     }
-    public static void main(String[] args) {
-        Pattern compile = Pattern.compile("\\d+");
-        String l = "44500";
-        boolean matches = l.matches(compile.pattern());
-        System.out.println(matches);
-        String s = "13213213131ddd/*sdas*/";
-        String s1 = s.replaceAll("/\\*\\w*\\*/", "");
-        System.out.println(s1);
+    public static void main(String[] args) throws IOException {
+//        Pattern compile = Pattern.compile("\\d+");
+//        String l = "44500";
+//        boolean matches = l.matches(compile.pattern());
+//        System.out.println(matches);
+//        String s = "13213213131ddd/*sdas*/";
+//        String s1 = s.replaceAll("/\\*\\w*\\*/", "");
+//        System.out.println(s1);
 
+        CompilationEngine compilationEngine = new CompilationEngine("E:\\nand2tetris\\nand2tetris-master\\nand2tetris-master\\projects\\10\\Square\\1\\Main.jack");
+        compilationEngine.parseClass();
     }
 
 }
